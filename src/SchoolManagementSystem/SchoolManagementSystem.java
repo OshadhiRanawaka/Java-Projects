1d73985655aa3d92fc17dc58b2b3fb096a9dcff6
Oshadhi <oshadhiranawaka@gmail.com>
2025-09-11 12:51:47 +0530

    Refactor main method in SchoolManagementSystem for improved readability and took an input from the user

--- src/SchoolManagementSystem/SchoolManagementSystem.java
+++ src/SchoolManagementSystem/SchoolManagementSystem.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class SchoolManagementSystem {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to CyberCrewz Academy\nSchool Management System V1\nCode name: River");
 
         while (true) {
             System.out.print("--Main Menu--");
-            System.out.println("\n1. Student Management\n2. Teacher Management\n3. Library Management\n4. Reports\n0. Exit");
+            System.out.println(
+                    "\n1. Student Management\n2. Teacher Management\n3. Library Management\n4. Reports\n0. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
 
         }
     }
