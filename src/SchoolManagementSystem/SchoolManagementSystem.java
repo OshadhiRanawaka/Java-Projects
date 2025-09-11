1e8e8a5aafc30c81b8ace5dee13183b0e00b7d5f
Oshadhi <oshadhiranawaka@gmail.com>
2025-09-11 12:37:12 +0530

    Update welcome message in SchoolManagementSystem class

--- src/SchoolManagementSystem/SchoolManagementSystem.java
+++ src/SchoolManagementSystem/SchoolManagementSystem.java
@@ -1,5 +1,5 @@
 public class SchoolManagementSystem {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Welcome to CyberCrewz Academy\nSchool Management System V1\nCode name: River");
     }
 }
