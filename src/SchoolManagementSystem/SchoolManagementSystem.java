e26b1347268f12e47a863069ce6b6693d15ab70f
Oshadhi <oshadhiranawaka@gmail.com>
2025-09-11 12:45:47 +0530

    Add main menu display to SchoolManagementSystem

--- src/SchoolManagementSystem/SchoolManagementSystem.java
+++ src/SchoolManagementSystem/SchoolManagementSystem.java
@@ -1,5 +1,11 @@
 public class SchoolManagementSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to CyberCrewz Academy\nSchool Management System V1\nCode name: River");
+
+        while (true) {
+            System.out.print("--Main Menu--");
+            System.out.println("\n1. Student Management\n2. Teacher Management\n3. Library Management\n4. Reports\n0. Exit");
+
+        }
     }
 }
