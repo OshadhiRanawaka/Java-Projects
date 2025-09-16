06d4fb4bf347c7663e5771bb4b8096aa1c003fdc
Oshadhi <oshadhiranawaka@gmail.com>
2025-09-16 12:49:15 +0530

    Enhance attendance management by adding student attendance report functionality and refining attendance marking logic

--- src/SchoolManagementSystem/utils/AttendanceManager.java
+++ src/SchoolManagementSystem/utils/AttendanceManager.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class AttendanceManager {
 
-    public static void attendanceManagementMenu(Scanner input, String[] studentNames, int[] studentIDs, String[][] attendance, int studentCount) {
+    public static void attendanceManagementMenu(Scanner input, String[] studentNames, int[] studentIDs,
+            String[][] attendance, int studentCount) {
         boolean mainMenu = true;
         while (mainMenu) {
             System.out.println("\n--- Attendance Management ---");
@@ -32,8 +33,7 @@ public class AttendanceManager {
                             attendance[i][dayIndex] = "P";
                         } else if (status.equalsIgnoreCase("A")) {
                             attendance[i][dayIndex] = "A";
-                        }
-                        else if (status.equalsIgnoreCase("-")) {
+                        } else if (status.equals("-")) {
                             attendance[i][dayIndex] = "-";
                         } else {
                             System.out.println("Invalid input. Please enter P or A.");
@@ -46,7 +46,7 @@ public class AttendanceManager {
 
                     System.out.println("+----------------------+----------+----------+----------+----------+");
                     System.out.printf("| %-20s | %-8s | %-8s | %-8s | %-8s |\n", "Name", "Day 1",
-                             "Day 2", "Day 3", "Day 4");
+                            "Day 2", "Day 3", "Day 4");
                     System.out.println("+----------------------+----------+----------+----------+----------+");
 
                     for (int i = 0; i < studentCount; i++) {
@@ -61,11 +61,50 @@ public class AttendanceManager {
                     System.out.println("(P = Present, A = Absent, - = Not Yet Marked)");
                 }
                 case 3 -> {
-                    System.out.println("Getting Student Attendance Report...");
+                    System.out.print("Enter the Student ID to check: ");
+                    int studentID = input.nextInt();
+                    int studentIndex = -1;
+                    for (int i = 0; i < studentCount; i++) {
+                        if (studentIDs[i] == studentID) {
+                            studentIndex = i;
+                            break;
+                        }
+                    }
+                    if (studentIndex == -1) {
+                        System.out.println("Student not found.");
+                    } else {
+                        System.out.println("Searching for " + studentNames[studentIndex] + "...");
+
+                        int presentCount = 0;
+                        int totalMarkedDays = 0;
+
+                        for (int j = 0; j < 4; j++) {
+                            if (!attendance[studentIndex][j].equals("-")) {
+                                totalMarkedDays++;
+                                if (attendance[studentIndex][j].equalsIgnoreCase("P")) {
+                                    presentCount++;
+                                }
+                            }
+                        }
+                        if (totalMarkedDays > 0) {
+                            double attendencePercentage = ((double) presentCount / totalMarkedDays) * 100;
+                            int absentCount = totalMarkedDays - presentCount;
+
+                            System.out.println("\n--- Attendace Report for " + studentNames[studentIndex] + " ---");
+                            System.out.println("Days Marked So Far: " + totalMarkedDays);
+                            System.out.println("Days Present: " + presentCount);
+                            System.out.println("Days Absent: " + absentCount);
+
+                            System.out.println("Attendance Percentage: " + attendencePercentage + "%");
+                        }
+
+                    }
                 }
+
                 case 4 -> {
                     System.out.println("Listing Absentees for a Day...");
                 }
+
                 case 0 -> {
                     mainMenu = false;
                     break;
